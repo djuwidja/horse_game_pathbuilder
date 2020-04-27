e752debbbc229b267526f477f6d29d97f39ca44e
kenneth <kenneth@kenneth-GX701GX-ubuntu>
2020-04-27 16:59:49 +0800

    Added Line2D to represent the line that this vector moves on.

--- src/main/java/com/djuwidja/horsegame/pathfinder/meta/path/vectorpath/VectorPathSect.java
+++ src/main/java/com/djuwidja/horsegame/pathfinder/meta/path/vectorpath/VectorPathSect.java
@@ -2,6 +2,7 @@ package com.djuwidja.horsegame.pathfinder.meta.path.vectorpath;
 
 import java.awt.geom.Point2D;
 
+import com.djuwidja.horsegame.pathfinder.math.Line2D;
 import com.djuwidja.horsegame.pathfinder.math.Vector2D;
 
 import lombok.Getter;
@@ -10,10 +11,12 @@ public class VectorPathSect {
 	@Getter private Point2D startPt;
 	@Getter private Point2D endPt;
 	@Getter private Vector2D vector;
+	@Getter private Line2D line;
 	
 	public VectorPathSect(Point2D startPt, Point2D endPt) {
 		this.startPt = startPt;
 		this.endPt = endPt;
 		this.vector = new Vector2D(startPt, endPt);
+		this.line = new Line2D(startPt, endPt);
 	}
 }

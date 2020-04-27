db9b9a6771e2914d6d35367e257405f063206316
kenneth <kenneth@kenneth-GX701GX-ubuntu>
2020-04-27 16:55:27 +0800

    Updated to use Artimetic.square().

--- src/main/java/com/djuwidja/horsegame/pathfinder/math/Parabola2D.java
+++ src/main/java/com/djuwidja/horsegame/pathfinder/math/Parabola2D.java
@@ -2,6 +2,7 @@ package com.djuwidja.horsegame.pathfinder.math;
 
 import lombok.Getter;
 import java.awt.geom.Point2D;
+import com.djuwidja.horsegame.pathfinder.math.Arithmetic;
 /**
  * Parabola class to model a path through the curved tracks within a race track. 
  * Note that the equation of this parabola is inverted (x = ay^2 + by + c) to be more fitting with its application.
@@ -28,22 +29,14 @@ public class Parabola2D {
 		this.evaluate();
 	}
 	/**
-	 * 
-	 * @param x
-	 * @return
-	 */
-	private static final double square(double x) {
-		return x*x;
-	}
-	/**
 	 * Evaluate the value of a, b and c.
 	 */
 	private void evaluate() {
 		double vxmtx = vertex.getX() - pt2.getX();
 		double tymoy = pt2.getY() - pt1.getY();
 		double vymty = vertex.getY() - pt2.getY();
-		double vy2mty2 = square(vertex.getY()) - square(pt2.getY());
-		double ty2moy2 = square(pt2.getY()) - square(pt1.getY());
+		double vy2mty2 = Arithmetic.square(vertex.getY()) - Arithmetic.square(pt2.getY());
+		double ty2moy2 = Arithmetic.square(pt2.getY()) - Arithmetic.square(pt1.getY());
 		
 		double au = vxmtx * tymoy - pt2.getX() * vymty + pt1.getX() * vymty;
 		double ad = vy2mty2 * tymoy - ty2moy2 * vymty;
@@ -55,7 +48,7 @@ public class Parabola2D {
 		
 		b = bu / bd;
 		
-		c = pt1.getX() - a * square(pt1.getY()) - b * pt1.getY();	
+		c = pt1.getX() - a * Arithmetic.square(pt1.getY()) - b * pt1.getY();	
 	}
 	/**
 	 * Get the value of x from y with ay^2 + by + c.
@@ -63,7 +56,7 @@ public class Parabola2D {
 	 * @return
 	 */
 	public double getX(double y) {
-		return a * square(y) + b * y + c;
+		return a * Arithmetic.square(y) + b * y + c;
 	}
 	/**
 	 * Get the slope of tangent in y.
@@ -89,15 +82,15 @@ public class Parabola2D {
 		
 		double bmMOn2a = (b - m) / (2 * a);
 		
-		double withinSqrt = Math.sqrt((cl - c) / a + square(bmMOn2a));
+		double withinSqrt = Math.sqrt((cl - c) / a + Arithmetic.square(bmMOn2a));
 		
 		double yPos = -bmMOn2a + withinSqrt;
 		double xPos = getX(yPos);
-		double distFromPtPos = square(pt.getX() - xPos) + square(pt.getY() - yPos);
+		double distFromPtPos = Arithmetic.square(pt.getX() - xPos) + Arithmetic.square(pt.getY() - yPos);
 				
 		double yNeg = -bmMOn2a - withinSqrt;
 		double xNeg = getX(yNeg);
-		double distFromPtNeg = square(pt.getX() - xNeg) + square(pt.getY() - yNeg);
+		double distFromPtNeg = Arithmetic.square(pt.getX() - xNeg) + Arithmetic.square(pt.getY() - yNeg);
 		
 		if (distFromPtNeg < distFromPtPos) {
 			return new Point2D.Double(xNeg, yNeg);

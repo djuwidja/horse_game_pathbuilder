f377bcc45c517f40ce7358cbff8ea1ccddf03211
kenneth <kenneth@kenneth-GX701GX-ubuntu>
2020-04-28 11:30:13 +0800

    TrackSectionCurve.getTangentVector now throws TrackSectionCurveException in case no tangent can be found.

--- src/main/java/com/djuwidja/horsegame/pathfinder/meta/curve/TrackSectionCurve.java
+++ src/main/java/com/djuwidja/horsegame/pathfinder/meta/curve/TrackSectionCurve.java
@@ -5,5 +5,5 @@ import java.awt.geom.Point2D;
 import com.djuwidja.horsegame.pathfinder.math.Vector2D;
 
 public interface TrackSectionCurve {
-	public Vector2D getTangentVector(Point2D pt);
+	public Vector2D getTangentVector(Point2D pt) throws TrackSectionCurveException;
 }
---
+++ src/main/java/com/djuwidja/horsegame/pathfinder/meta/curve/TrackSectionCurveException.java
@@ -0,0 +1,12 @@
+package com.djuwidja.horsegame.pathfinder.meta.curve;
+
+public class TrackSectionCurveException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TrackSectionCurveException(String errorMsg) {
+		super(errorMsg);
+	}
+}

83e39c783cccf651f40ae1fa919dcf05cb4f5b0f
kenneth <kenneth@kenneth-GX701GX-ubuntu>
2020-05-06 15:33:31 +0800

    Added maintain speed AI implementation.

--- src/main/java/com/djuwidja/horsegame/pathfinder/ai/RaceHorseAI.java
+++ src/main/java/com/djuwidja/horsegame/pathfinder/ai/RaceHorseAI.java
@@ -14,15 +14,52 @@ public class RaceHorseAI implements AI {
 	@Getter Vector2D moveVec;
 	@Getter Vector2D moveNor;
 	
+	private double normalFwdSpd;
+	private double normalAngSpd;
+	private double curSpd;
+	private HorseAIState state;
+	
 	public RaceHorseAI(RaceHorse raceHorse, StartPoint startPoint) {
 		this.raceHorse = raceHorse;
 		this.position = startPoint.getStartPos();
 		this.moveVec = startPoint.getStartVec();
 		this.moveNor = moveVec.normal();
+		
+		this.state = HorseAIState.MAINTAIN_SPEED;
+		this.normalFwdSpd = raceHorse.getFwdSpdMax() * 0.8d;
+		this.normalAngSpd = raceHorse.getAngSpdMax() * 0.8d;
+		this.curSpd = 0d;
 	}
 
 	@Override
 	public void update(double timeDelta) {
+		switch (state) {
+		case MAINTAIN_SPEED:
+			maintainSpeed(timeDelta);
+			break;
+		default:
+			break;
+		}
+	}
 		
+	private void maintainSpeed(double timeDelta) {
+		double normalSpd = 0d;
+		double acc = 0d;
+		if (Math.abs(moveVec.getX()) >= 0.95d) {
+			normalSpd = this.normalFwdSpd * timeDelta;
+			acc = raceHorse.getFwdAcc() * timeDelta;
+		} else {
+			normalSpd = this.normalAngSpd * timeDelta;
+			acc = raceHorse.getAngAcc() * timeDelta;
+		}
+		
+		if (curSpd < normalSpd) {
+			curSpd += acc;
+		} else {
+			curSpd = normalSpd;
+		}
+		
+		this.position.setLocation(this.position.getX() + this.moveVec.getX() * curSpd, 
+								  this.position.getY() + this.moveVec.getY() * curSpd);
 	}
 }
